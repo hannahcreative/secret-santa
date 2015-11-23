e589f5fca2556cf678f4399c149f9cbba5a47096
Hannah <hello@hannahallen.ca>
2015-11-23 18:31:35 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,5 @@
+// 1. user provides all email address at once, and they are inputted to an array, which is randomized and returns one new email address to each person 
+
+// 2. user also set's gift $ limit. (Which is stored and included in the email)
+
+// 3. a random email address variable is sent to each email in the array
